a64c9cd5234dffdfcd1d59ba1fddf40e6d31601d
Mikhail Bautin <mikhail@yugabyte.com>
2018-09-03 10:43:46 -0700

    Update version in Version.java

--- src/main/java/junit/runner/Version.java
+++ src/main/java/junit/runner/Version.java
@@ -9,7 +9,7 @@ public class Version {
 	}
 
 	public static String id() {
-		return "4.13-master-02c3280-yb-1-SNAPSHOT";
+		return "4.13-master-02c3280-yb-3-SNAPSHOT";
 	}
 	
 	public static void main(String[] args) {
